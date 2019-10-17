385e1fb62eefc89d1675e5f40bfe64e4b4378b51
Sergey IRISOV <sergey.irisov@al-enterprise.com>
2019-10-17 14:31:24 +0300

    Some refactor long time ago.

--- core/src/org/mie/game/MIGame.java
+++ core/src/org/mie/game/MIGame.java
@@ -1,21 +1,23 @@
 package org.mie.game;
 
 
+
+import org.mie.game.model.Island;
+import org.mie.game.view.IslandRenderer;
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class MIGame extends ApplicationAdapter {
 
   private float elapsedTime = 0;
   
-  private final ShapeRenderer renderer = new ShapeRenderer();
+  private IslandRenderer renderer;
+  private Island island;
 
   @Override
   public void create() {
+    island = new Island();
+    renderer = new IslandRenderer(island, 800, 600);
   }
 
   @Override
@@ -23,15 +25,19 @@ public class MIGame extends ApplicationAdapter {
     // Advance the world, by the amount of time that has elapsed since the last frame
     // Generally in a real game, dont do this in the render loop, as you are tying the physics
     // update rate to the frame rate, and vice versa
-    world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+    //world.step(Gdx.graphics.getDeltaTime(), 6, 2);
 
-    elapsedTime += Gdx.graphics.getDeltaTime();
+    //elapsedTime += Gdx.graphics.getDeltaTime();
     // You know the rest...
-    Gdx.gl.glClearColor(1, 1, 1, 1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    renderer.begin(ShapeType.Filled);
-    renderer.setColor(new Color(0, 0, 0, 1));
-    map.forEach((l) -> renderer.rect(elapsedTime, elapsedTime, elapsedTime, elapsedTime));
-    renderer.end();
+
+    renderer.render();
   }
+
+  @Override
+  public void dispose() {
+    island.dispose();
+  }
+  
+  
+
 }
--- desktop/src/org/mie/game/desktop/DesktopLauncher.java
+++ desktop/src/org/mie/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
                 config.title = "Misterious Island: Escape";
-                config.width = 1024;
+                config.width = 800;
                 config.height = 600;
 		new LwjglApplication(new MIGame(), config);
 	}
